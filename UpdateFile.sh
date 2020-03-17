c1d38d89da18d06b2d9d86ca8975e7039b372cd8
Kirant50 <60968631+Kirant50@users.noreply.github.com>
2020-03-17 15:59:25 +0530

    Update UpdateFile.sh

--- UpdateFile.sh
+++ UpdateFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Initiate file update'
-dos2unix testdefault
+dos2unix UpdateFile.sh testdefault
 export folderpath=$(pwd)/testdefault
 echo "Path : $folderpath"
 sed '$aimportType=DATABASE' $folderpath
