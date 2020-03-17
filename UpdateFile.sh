0903352de161716d60b5fce0b20ded63412ebbf0
Kirant50 <60968631+Kirant50@users.noreply.github.com>
2020-03-17 12:39:39 +0530

    Update UpdateFile.sh

--- UpdateFile.sh
+++ UpdateFile.sh
@@ -1,4 +1,4 @@
 echo 'Initiate file update'
-folderpath=$(pwd)
-echo "Path : $folderpath/testdefault"
-sed '$aimportType=DATABASE' $folderpath/testdefault
+export folderpath=$(pwd)/testdefault
+echo "Path : $folderpath"
+sed '$aimportType=DATABASE' $folderpath
